dd053e05b74a99f6e14ce65621617119d16ca57a
Ampgh Tambad <amoghtambad@gmail.com>
2024-03-28 14:27:53 -0700

    Still figuring out S3 output structure

--- handler.py
+++ handler.py
@@ -27,9 +27,3 @@ def handler(event, context):
     # fps = subprocess.check_output(fps_cmd, shell=True).decode("utf-8").rstrip("\n")
     # fps = math.ceil(float(fps))
     # return outdir
-
-{'Records': [{'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'us-east-1', 'eventTime': '2024-03-27T21:38:39.475Z', 
-              'eventName': 'ObjectCreated:Put', 'userIdentity': {'principalId': 'A2OTPLWQ08QZC3'}, 'requestParameters': {'sourceIPAddress': '129.219.21.44'}, 
-              'responseElements': {'x-amz-request-id': '5SK7F01TKX1BEQ50', 'x-amz-id-2': 'czj4AUlJS99oXRwwO/P4iXOaqK2bktpyHK3i4h0Ofg5alWlOyubtabgf+4ytL1MGTMH5gNY05onbBhT5o8+SndgdTSCc4lQw'}, 
-              's3': {'s3SchemaVersion': '1.0', 'configurationId': 'ec601d40-003e-4c0d-97a4-c74ad3989093', 'bucket': {'name': '1229560048-input', 'ownerIdentity': {'principalId': 'A2OTPLWQ08QZC3'}, 
-                'arn': 'arn:aws:s3:::1229560048-input'}, 'object': {'key': 'test_2.mp4', 'size': 418298, 'eTag': '400ca61b39ea2d349a65e6b03d52fae4', 'sequencer': '00660491DF5C94558C'}}}]}

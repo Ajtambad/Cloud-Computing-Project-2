ed02d95dc69ae50855cafe76e054de6f9f8ddcde
Ampgh Tambad <amoghtambad@gmail.com>
2024-03-28 19:05:02 -0700

    Testing it with lambda by uploading file to S3

--- handler.py
+++ handler.py
@@ -11,16 +11,14 @@ input_bucket = '1229560048-input'
 
 s3 = boto3_client('s3', region_name='us-east-1')
 
-# def handler(event, context):
-def handler():
+def handler(event, context):
 
-    # video_filename = event['Records'][0]['s3']['object']['key']
-    video_filename = 'test_2.mp4'
-    file = s3.download_file(input_bucket, video_filename, video_filename)
+    video_filename = event['Records'][0]['s3']['object']['key']
+    file = s3.download_file(input_bucket, video_filename, video_filename) #Download from input S3 bucket
     filename = os.path.basename(video_filename)
     outdir = os.path.splitext(filename)[0]
     outdir = os.path.join("/tmp",outdir)
-    
+
     # s3.put_object(Key=(filename.split('.')[0] + '/'))
     if not os.path.exists(outdir):
         os.makedirs(outdir)
@@ -32,6 +30,7 @@ def handler():
         print(e.returncode)
         print(e.output)
 
+    #Upload to Stage-1 S3 Bucket
     dir = os.listdir(outdir)
     for file in dir:
         s3.upload_file(outdir + '/' + file, stage_1_bucket, filename.split('.')[0] + '/{}'.format(file))
@@ -39,9 +38,4 @@ def handler():
     fps_cmd = 'ffmpeg -i ' + video_filename + ' 2>&1 | sed -n "s/.*, \\(.*\\) fp.*/\\1/p"'
     fps = subprocess.check_output(fps_cmd, shell=True).decode("utf-8").rstrip("\n")
     fps = math.ceil(float(fps))
-    print(outdir)
-    print(fps)
     return outdir
-
-if __name__=='__main__':
-    handler()
